fcbaf80be2568335fb22cd5cebccb5c20432c776
qyzhao <qyzhao@thoughtworks.com>
2018-12-11 11:04:02 +0800

    [qy] add heroes list.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Captain American'},
+  { id: 2, name: 'Iron Man' },
+  { id: 3, name: 'Thor' },
+  { id: 4, name: 'Spider Man' },
+  { id: 5, name: 'Doctor Strange' },
+  { id: 6, name: 'Hulk' },
+  { id: 7, name: 'Vision' },
+  { id: 8, name: 'Hawkeye' },
+  { id: 9, name: 'Black Widow' },
+  { id: 10, name: 'Star Lord' },
+  { id: 11, name: 'Winter Soldier' },
+  { id: 13, name: 'Erik Killmonger' },
+];
