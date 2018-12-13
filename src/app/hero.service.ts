8847dfd054a2d90f8a08afda4d503e8763a54976
qyzhao <qyzhao@thoughtworks.com>
2018-12-13 15:34:48 +0800

    [qy] use observable for get data asynchronous.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
-import {Hero} from './hero';
-import {HEROES} from './mock-heroes';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { of } from 'rxjs/observable/of';
+import { Observable } from 'rxjs/observable';
 
 @Injectable()
 export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
